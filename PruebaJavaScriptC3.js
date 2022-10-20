6908612f796d1f3fb3d35dba6ed1c66c50e3aef0
diegogatica369 <diegogaticaretamales@gmail.com>
2022-10-20 10:55:43 -0300

    PruebaJavaScriptC3DiegoGatica

---
+++ PruebaJavaScriptC3.js
@@ -0,0 +1,50 @@
+//ejercicio 1
+/*Don José todos los martes y jueves realiza un 20% de descuento en el total de las
+compras, crea una función la cual debe recibir un parámetro del día y el arreglo de
+valores de los productos a comprar, esta debe retornar el total final de la compra según
+corresponda o no descuento*/
+
+var martes = 1
+var jueves = 1
+var array = [200, 300, 400]
+function descuento (aplicado)
+if 
+
+
+
+//ejercicio 2
+/* Don José hace un 5% de descuento a los clientes que compran más de 3 productos y al
+menos uno de ellos tiene un valor mayor a 10.000, realiza una función que calcule el
+total verificando si corresponde o no el descuento.
+*/
+
+//verificar si el cliente compro mas de 3 productos
+//uno de ellos tiene el valor de 10.000?
+//verificar si corresponde el descuento
+
+
+//ejercico 3 
+/* Se desea tener una función verificadora encargada de revisar si dentro de el arreglo de
+valores de los productos a comprar no existan valores negativos ingresados por error,
+en caso de no existir debe retornar un mensaje de éxito, en caso contrario debe retornar
+un mensaje de error junto con el número negativo y el índice en el que se encontraba.*/
+var lista = [700, 1100, 2300, 3000]
+function verificar (array) {
+if(lista > 0){
+    console.log("bien, tus valores estan bien ingresados")
+}else if(lista < 0) {
+    console.log("error")
+}
+    
+}
+
+//ejercicio 4
+/* Dado el siguiente arreglo [200,5500,900,7000,500,300] con los valores de cada
+producto
+○ Crea una función que retorne el valor del producto más costoso.
+○ Crea una función que retorne el valor del producto menos costoso. */
+
+var array = [200,5500,900,7000,500,300];
+
+function retorno (valormascostoso);
+array.
